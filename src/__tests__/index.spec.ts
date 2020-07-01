16c448db13a487543b15660a9bbad650d28d487a
Guilherme Caulada <guistoppa1995@gmail.com>
2020-07-01 20:29:34 -0300

    wip: add unitary tests for jshelix

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,7 +1,22 @@
-import * as m from "../index";
+import jsHelix from "../index";
 
-describe("module name", () => {
-  it("testMe", () => {
-    expect(m.testMe(1, 2)).toEqual(3);
+describe("index", () => {
+  it("Calls default jsHelix mock with clientId and token", () => {
+    const hapi = jsHelix("clientId", "token");
+    expect(hapi.url).toEqual("https://api.twitch.tv");
+    expect(hapi.headers).toEqual({
+      "Client-ID": "clientId",
+      "Content-Type": "application/json",
+      Authorization: "Bearer token",
+    });
+  });
+
+  it("Calls default jsHelix mock with only clientId", () => {
+    const hapi = jsHelix("clientId");
+    expect(hapi.url).toEqual("https://api.twitch.tv");
+    expect(hapi.headers).toEqual({
+      "Client-ID": "clientId",
+      "Content-Type": "application/json",
+    });
   });
 });
